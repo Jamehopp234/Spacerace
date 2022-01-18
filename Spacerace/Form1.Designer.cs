c15a707ecae5f4dfd448b41d13af5eeff7038c49
jamehopp234 <jamehopp234@ed.amdsb.ca>
2022-01-18 14:24:03 -0500

    Add project files.

---
+++ Spacerace/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Spacerace
+{
+    partial class Spacerace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Spacerace));
+            this.P1ScoreLabel = new System.Windows.Forms.Label();
+            this.P2ScoreLabel = new System.Windows.Forms.Label();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
+            this.yesButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // P1ScoreLabel
+            // 
+            this.P1ScoreLabel.AutoSize = true;
+            this.P1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.P1ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.P1ScoreLabel.Location = new System.Drawing.Point(12, 527);
+            this.P1ScoreLabel.Name = "P1ScoreLabel";
+            this.P1ScoreLabel.Size = new System.Drawing.Size(70, 25);
+            this.P1ScoreLabel.TabIndex = 0;
+            this.P1ScoreLabel.Text = "label1";
+            // 
+            // P2ScoreLabel
+            // 
+            this.P2ScoreLabel.AutoSize = true;
+            this.P2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.P2ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.P2ScoreLabel.Location = new System.Drawing.Point(460, 527);
+            this.P2ScoreLabel.Name = "P2ScoreLabel";
+            this.P2ScoreLabel.Size = new System.Drawing.Size(70, 25);
+            this.P2ScoreLabel.TabIndex = 1;
+            this.P2ScoreLabel.Text = "label1";
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Interval = 20;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(171, 9);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(70, 25);
+            this.winLabel.TabIndex = 2;
+            this.winLabel.Text = "label1";
+            // 
+            // yesButton
+            // 
+            this.yesButton.BackColor = System.Drawing.Color.Transparent;
+            this.yesButton.Enabled = false;
+            this.yesButton.FlatAppearance.BorderSize = 0;
+            this.yesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yesButton.ForeColor = System.Drawing.Color.White;
+            this.yesButton.Location = new System.Drawing.Point(143, 67);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 3;
+            this.yesButton.Text = "YES";
+            this.yesButton.UseVisualStyleBackColor = false;
+            this.yesButton.Visible = false;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // noButton
+            // 
+            this.noButton.BackColor = System.Drawing.Color.Transparent;
+            this.noButton.Enabled = false;
+            this.noButton.FlatAppearance.BorderSize = 0;
+            this.noButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.noButton.ForeColor = System.Drawing.Color.White;
+            this.noButton.Location = new System.Drawing.Point(367, 67);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(75, 23);
+            this.noButton.TabIndex = 4;
+            this.noButton.Text = "NO";
+            this.noButton.UseVisualStyleBackColor = false;
+            this.noButton.Visible = false;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.Enabled = false;
+            this.playButton.FlatAppearance.BorderSize = 0;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(262, 67);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 5;
+            this.playButton.Text = "PLAY";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Visible = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // Spacerace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.P2ScoreLabel);
+            this.Controls.Add(this.P1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Spacerace";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Space Race";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Spacerace_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Spacerace_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Spacerace_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label P1ScoreLabel;
+        private System.Windows.Forms.Label P2ScoreLabel;
+        private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.Button playButton;
+    }
+}
+
---
+++ Spacerace/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Spacerace
+{
+    public partial class Spacerace : Form
+    {
+        SoundPlayer Explosion = new SoundPlayer(Properties.Resources.Explosion);
+        SoundPlayer Victory = new SoundPlayer(Properties.Resources.VictorySound);
+
+        Rectangle player1 = new Rectangle(200, 550, 40, 10);
+        int player1Speed = 10;
+        Rectangle player2 = new Rectangle(400, 550, 40, 10);
+        int player2Speed = 10;
+
+        List<Rectangle> balls = new List<Rectangle>();
+        List<Rectangle> ballsRight = new List<Rectangle>();
+        List<int> ballSpeeds = new List<int>();
+        List<string> ballColours = new List<string>();
+        int ballSize = 10;
+
+        int P1Score = 0;
+        int P2Score = 0;
+
+
+        bool downDown = false;
+        bool upDown = false;
+        bool keyUp = false;
+        bool keyDown = false;
+
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+        
+
+        
+
+
+        public Spacerace()
+        {
+             
+            InitializeComponent();
+
+            winLabel.Text = "Welcome To Space Race \n Click Play To Begin";
+            playButton.Visible = true;
+            playButton.Enabled = true;
+        }
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            playButton.Visible = false;
+            playButton.Enabled = false;
+            yesButton.Visible = false;
+            yesButton.Enabled = false;
+            noButton.Visible = false;
+            noButton.Enabled = false;
+            winLabel.Text = "";
+
+            if (upDown == true && player1.Y < 550 - player1.Height)
+            {
+                player1.Y += player1Speed;
+                
+            }
+            if (downDown == true && player1.Y > 0)
+            {
+                player1.Y -= player1Speed;
+               
+            }
+
+            if (keyUp == true && player2.Y < 550 - player2.Height)
+            {
+                player2.Y += player2Speed;
+               
+            }
+            if (keyDown == true && player2.Y > 0)
+            {
+                player2.Y -= player2Speed;
+                
+            }
+
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                //find the new postion of y based on speed 
+                int x = balls[i].X + ballSpeeds[i];
+
+                //replace the rectangle in the list with updated one using new y 
+                balls[i] = new Rectangle(x, balls[i].Y, ballSize, ballSize);
+            }
+            for (int i = 0; i < ballsRight.Count(); i++)
+            {
+                //find the new postion of y based on speed 
+                int x = ballsRight[i].X - ballSpeeds[i];
+
+                //replace the rectangle in the list with updated one using new y 
+                ballsRight[i] = new Rectangle(x, ballsRight[i].Y, ballSize, ballSize);
+            }
+
+
+            randValue = randGen.Next(0, 101);
+
+            if (randValue < 11)
+            {
+                int y = randGen.Next(10, this.Height - ballSize * 6);
+                balls.Add(new Rectangle(10, y, ballSize, ballSize));
+                ballSpeeds.Add(randGen.Next(2, 10));
+
+                ballColours.Add("white");
+            }
+            if (randValue < 11)
+            {
+                int y = randGen.Next(10, this.Height - ballSize * 6);
+                ballsRight.Add(new Rectangle(600, y, ballSize, ballSize));
+                ballSpeeds.Add(randGen.Next(2, 10));
+
+                ballColours.Add("white");
+            }
+
+            //check if ball is below play area and remove it if it is 
+            for (int i = 0; i < balls.Count(); i++)
+            {
+
+
+                if (balls[i].X > this.Width - ballSize)
+                {
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+
+                }
+            }
+            for (int i = 0; i < ballsRight.Count(); i++)
+            {
+
+
+                if (ballsRight[i].X < 0)
+                {
+                    ballsRight.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+
+                }
+            }
+
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                if (player1.IntersectsWith(balls[i]))
+                {
+                    player1.Y = 550;
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    Explosion.Play();
+
+                }
+                if (player2.IntersectsWith(balls[i]))
+                {
+                    player2.Y = 550;
+                    balls.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    Explosion.Play();
+
+                }
+            }
+            for (int i = 0; i < ballsRight.Count(); i++)
+            {
+                if (player1.IntersectsWith(ballsRight[i]))
+                {
+                    player1.Y = 550;
+                    ballsRight.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+
+                }
+                if (player2.IntersectsWith(ballsRight[i]))
+                {
+                    player2.Y = 550;
+                    ballsRight.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+
+                }
+            }
+
+            if (player1.Y == 0)
+            {
+                P1Score++;
+                player1.Y = 550;
+            }
+            if (player2.Y == 0)
+            {
+                P2Score++;
+                player2.Y = 550;
+            }
+
+            if (P1Score == 3)
+            {
+                Victory.Play();
+                gameLoop.Stop();
+                winLabel.Text = "Player 1 Wins    Play Again?";
+                yesButton.Visible = true;
+                yesButton.Enabled = true;
+                noButton.Visible = true;
+                noButton.Enabled = true;
+            }
+            if(P2Score == 3)
+            {
+                Victory.Play();
+                gameLoop.Stop();
+                winLabel.Text = "Player 2 Wins    Play Again?";
+                yesButton.Visible = true;
+                yesButton.Enabled = true;
+                noButton.Visible = true;
+                noButton.Enabled = true;
+                
+                
+            }
+
+
+            Refresh();
+        }
+
+        private void Spacerace_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.S:
+                    upDown = true;
+                    break;
+                case Keys.W:
+                    downDown = true;
+                    break;
+                case Keys.Down:
+                    keyUp = true;
+                    break;
+                case Keys.Up:
+                    keyDown = true;
+                    break;
+            }
+        }
+
+        private void Spacerace_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.S:
+                    upDown = false;
+                    break;
+                case Keys.W:
+                    downDown = false;
+                    break;
+                case Keys.Down:
+                    keyUp = false;
+                    break;
+                case Keys.Up:
+                    keyDown = false;
+                    break;
+            }
+        }
+
+        private void Spacerace_Paint(object sender, PaintEventArgs e)
+        {
+
+            P1ScoreLabel.Text = $"P1 Score: {P1Score}";
+            P2ScoreLabel.Text = $"P2 Score: {P2Score}";
+
+
+
+            //draw hero 
+            e.Graphics.FillRectangle(whiteBrush, player1);
+            e.Graphics.FillRectangle(whiteBrush, player2);
+
+            //draw balls 
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                if (ballColours[i] == "white")
+                {
+                    e.Graphics.FillEllipse(whiteBrush, balls[i]);
+                }
+
+            }
+            for (int i = 0; i < ballsRight.Count(); i++)
+            {
+                if (ballColours[i] == "white")
+                {
+                    e.Graphics.FillEllipse(whiteBrush, ballsRight[i]);
+                }
+
+            }
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            yesButton.Visible = false;
+            yesButton.Enabled = false;
+            noButton.Visible = false;
+            noButton.Enabled = false;
+            winLabel.Text = "";
+            gameLoop.Start();
+            P1Score = 0;
+            P2Score = 0;
+            P1ScoreLabel.Text = $"P1 Score: {P1Score}";
+            P2ScoreLabel.Text = $"P2 Score: {P2Score}";
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            gameLoop.Start();
+        }
+    }
+
+}
+
+
+
